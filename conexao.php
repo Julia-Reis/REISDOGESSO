d65a219a68d6178e2551a5e462ff3b08065de1df
Julia-Reis <60949284+Julia-Reis@users.noreply.github.com>
2021-01-16 09:55:29 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,15 @@
 <?php
+/*
     define('HOST', 'localhost');
     define('USUARIO', 'root');
+    define('SENHA', '@Abracadabra123');
+    define('DB', 'reisdogesso');
+
+    $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+*/
+
+    define('HOST', 'localhost');
+    define('USUARIO', 'juliareis');
     define('SENHA', '');
     define('DB', 'reisdogesso');
 
